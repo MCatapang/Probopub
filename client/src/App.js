6c8230e61fcf887727cbad42f97efc1b48de9167
MCatapang <maocatap@gmail.com>
2022-07-20 18:11:32 -0700

    Established color palette

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Filler from './components/Filler';
 import Test from './components/Test';
+import styles from './App.css';
 
 const App = () => {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path='*' element={<Filler />} />
                 <Route path='/test' element={<Test />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = (props) => {
+
+    return (
+        <h1 id="navbar">This is the Navbar</h1>
+    )
+}
+
+export default Navbar;
