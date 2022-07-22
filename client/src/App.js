51d3e1df211d90e31bc20cecb25430aed5b940a1
MCatapang <maocatap@gmail.com>
2022-07-21 18:12:24 -0700

    Began incorporating tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,6 @@ import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
 import Register from './components/Register';
-import styles from './App.css';
-
 
 const App = () => {
     return (
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Welcome = (props) => {
     return (
-        <>
+        <div className="bg-custom-bg-body">
             <h1>This is the welcome page</h1>
-        </>
+        </div>
     )
 }
 
