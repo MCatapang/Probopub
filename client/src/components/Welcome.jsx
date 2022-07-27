7a561997b826af86d82b26a29fc0a7f9758714bd
MCatapang <maocatap@gmail.com>
2022-07-27 14:07:10 -0700

    Created homeBanner

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 const Welcome = (props) => {
+    let items = [];
+    for(let i=0; i<200; i++) {
+        items.push(<h1>Hello world</h1>)
+    }
+
     return (
-        <div>
-            <h1>This is the welcome page</h1>
+        <div className='content'>
+            <div className='homeBanner'>
+                <h1>
+                    Learn 
+                    <br/>
+                    case law 
+                    <br/>
+                    your way
+                </h1>
+            </div>
+            {items}
         </div>
     )
 }
