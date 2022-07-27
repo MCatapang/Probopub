fb4c5377ed62677fe717a944b6c85d11b3a1eab6
MCatapang <maocatap@gmail.com>
2022-07-26 19:39:27 -0700

    Finished styling navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import logo from '../assets/images/logo.png';
 
 const Navbar = (props) => {
     const navigate = useNavigate()
-
+    
     return (
         <div id="navbar">
-            <h1 id="navbarLogo">Probopub</h1>
+            <h1 id="navbarHeader" onClick={() => navigate('/')}>
+                <img src={logo} alt="logo"/>
+                Probopub
+            </h1>
             <ul id="navbarLinks">
-                <li onClick={() => navigate('/login')}>Login</li>
-                <li onClick={() => navigate('/register')}>Register</li>
+                <li onClick={() => navigate('/login')}>
+                    <h2>Login</h2>
+                </li>
+                <li onClick={() => navigate('/register')}>
+                    <h2>Register</h2>
+                </li>
             </ul>
         </div>
     )
